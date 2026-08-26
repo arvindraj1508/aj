42da600a3531a3f7ec717b5e3bae49c2803633d6
arvindraj1508 <arvindraj1503@gmail.com>
2026-08-26 16:57:30 +0530

    Add files via upload

---
+++ general/naturalsum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int i,n,sum;
+  sum=0;
+  printf("enter the value of n:");
+  scanf("%d",&n);
+  for(n==0;sum+n;n++)
+	  printf("sum of the %d numbers:%d\n",n,sum);
+	 scanf("%d%d",&n,&sum);
+  return 0;
+}
