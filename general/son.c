e9011fe55d90b1317846c05e88b84f69cff7cd1a
arvindraj1508 <arvindraj1503@gmail.com>
2026-08-26 16:55:57 +0530

    Add files via upload

---
+++ general/son.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,sum;
+	printf("enter the number:");
+	scanf("%d",&n);
+	for(i==0;i=n+i;i++)
+	{
+		printf("sum of natural numbers");
+		sum=n+i;
+		scanf("%d",&sum);
+		printf("%d+%d=%d\n",n,i,sum);
+	  }
+	return 0;
+}
