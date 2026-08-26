d586d4af7a0ac08d2ec29fb579bf2af4e03d6f5f
arvindraj1508 <arvindraj1503@gmail.com>
2026-08-26 16:54:28 +0530

    Add files via upload

---
+++ general/sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,sub;
+	printf("enter the value:");
+	scanf("%d%d",&n,&m);
+	sub=n-m;
+	printf("the sub value is %d\n",sub);
+	scanf("%d",&sub);
+	return 0;
+}
